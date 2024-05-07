69e6d7adb068e45319d3ab1a7ab57d0d009876f0
ar4rpon <hsgw28tech@gmail.com>
2024-05-08 03:55:51 +0900

    initial commit

---
+++ src/main/java/practice/ReactSpringBootTodoApp/ReactSpringBootTodoAppApplication.java
@@ -0,0 +1,13 @@
+package practice.ReactSpringBootTodoApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactSpringBootTodoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactSpringBootTodoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/practice/ReactSpringBootTodoApp/ReactSpringBootTodoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package practice.ReactSpringBootTodoApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactSpringBootTodoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
